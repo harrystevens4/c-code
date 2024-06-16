5a44f155b12879549b83daf36f12b24b986f4811
rufus173 <derrickotron5000@gmail.com>
2024-06-16 13:23:36 +0100

    made the program complete its funciton and print errors to standard error.

--- source-code/remove-substring.c
+++ source-code/remove-substring.c
@@ -41,21 +41,45 @@ int *getstringpos(char string[],char substring[]) {
 	return result;
 }
 
+char *remove_substring(char string[],int start,int end){
+	int length = str_len(string)-(+end-start+1);
+	//printf("new string length:%d\n",length);
+	char result[length+1];
+	result[length] = '\0';
+	int count = 0;
+	for (int i=0;i<str_len(string);i++){
+		//printf("%c\n",string[i]);
+		if ( ! (i >= start && i<= end)) {
+			result[count] = string[i];
+			count++;
+		}
+
+	}
+	//printf("result:%s\n",result);
+	char *end_result = malloc(length+1); //+1 for the '\0' at the end
+	strcpy(end_result,result);
+	return end_result;
+}
+
 void main(int argc, char* argv[]){
 	int i;
 	if (argc != 3){
-		printf("expected 2 arguments, but got %d",argc-1);
+		fprintf(stderr,"expected 2 arguments, but got %d\n",argc-1);
 	}else{
 		char string[100];
 		strcpy(string,argv[1]);
 		char substring[100];
 		strcpy(substring,argv[2]);
-		printf("string:%s\n",string);
-		printf("substring:%s\n",substring);
+		//printf("string:%s\n",string);
+		//printf("substring:%s\n",substring);
 		int *result;
 		result = getstringpos(string,substring);
-		printf("start:%d,end:%d\n",result[0],result[1]);
+		//printf("start:%d,end:%d\n",result[0],result[1]);
+		char *new_string;
+		new_string = remove_substring(string,result[0],result[1]);
+		printf("%s\n",new_string);
 		free(result);
+		free(new_string);
 	}
 }
 
