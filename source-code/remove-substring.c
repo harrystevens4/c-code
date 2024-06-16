dd2cba917ae45164d31d0d80d1ae29b9498f2971
rufus173 <derrickotron5000@gmail.com>
2024-06-16 12:47:57 +0100

    fixed issues with the substring position detection

--- source-code/remove-substring.c
+++ source-code/remove-substring.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 int str_len(char string[]){
 	int count = 0;
@@ -10,7 +11,7 @@ int str_len(char string[]){
 		}
 	}
 }
-char getstringpos(char string[],char substring[]) {
+int *getstringpos(char string[],char substring[]) {
 	int i;	
 	int j;
 	int substring_length;
@@ -19,18 +20,25 @@ char getstringpos(char string[],char substring[]) {
 	int end;
 	substring_length=str_len(substring);
 	string_length=str_len(string);
-	printf("strlen:%d,substrlen:%d\n",substring_length,string_length);
+	//printf("strlen:%d,substrlen:%d\n",substring_length,string_length);
 	for (i=0;i<string_length;i++){
-		start=i;
 		for (j=0;j<substring_length;j++){
+			//printf("comparing %c to %c\n",string[i],substring[j]);
 			if (string[i]==substring[j]){
-				end=j;
+				end=i;
+				start=i-substring_length+1;
+				//printf("letter match!\n");
 			}else{
 				break;
 			}
+		i++;
 		}
 	}
-	printf("start:%d,end:%d\n",start,end);
+	//printf("start:%d,end:%d\n",start,end);
+	int *result = malloc(1024);
+	result[0]=start;
+	result[1]=end;
+	return result;
 }
 
 void main(int argc, char* argv[]){
@@ -44,8 +52,10 @@ void main(int argc, char* argv[]){
 		strcpy(substring,argv[2]);
 		printf("string:%s\n",string);
 		printf("substring:%s\n",substring);
-		int start;
-		start = getstringpos(string,substring);
+		int *result;
+		result = getstringpos(string,substring);
+		printf("start:%d,end:%d\n",result[0],result[1]);
+		free(result);
 	}
 }
 
