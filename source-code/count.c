5a413a7a59f165f726fe0d93b8d27871ff3aca01
rufus173 <derrickotron5000@gmail.com>
2024-06-25 19:43:42 +0100

    added -w flag for counting words

--- source-code/count.c
+++ source-code/count.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include "args.h" //compile with args.c
 int letter_count(struct args arguments);
+int word_count(struct args arguments);
 int main(int argc,char *argv[]){
 	if (argc==1){
 		printf("0\n");//0 letters / words
@@ -13,7 +14,7 @@ int main(int argc,char *argv[]){
 	int i = 0;
 	for (i=0;i<arguments.number_single;i++){
 		if (arguments.single[i] == 'w'){
-			;
+			printf("%d\n",word_count(arguments));
 		}else{
 			;
 		}
@@ -22,6 +23,9 @@ int main(int argc,char *argv[]){
 		printf("%d\n",letter_count(arguments));
 	}
 
+	for (i=0;i<arguments.number_other;i++){
+		free(arguments.other[i]);
+	}
 	free(arguments.other); //freeing from the arg parser
 }
 
@@ -29,6 +33,20 @@ int letter_count(struct args arguments){
 	int total = arguments.number_other-1;
 	for (int i=0;i<arguments.number_other;i++){
 		total += (int)strlen(arguments.other[i]);
+	}
+	return total;
+}
+int word_count(struct args arguments){
+	int total = 0;
+	if (arguments.number_other != 0){
+		total = 1;
+	}else{
+		return 0;
+	}
+	for (int i = 0;i<strlen(arguments.other[0]);i++){// for each letter in the fisrt string argument
+		if (arguments.other[0][i] == ' '){ //we check for the number of spaces to count words
+			total++;
+		}
 	}
 	return total;
 }
