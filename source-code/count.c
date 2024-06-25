040c352d544d83d0bc6a1702c69bb58a09e41977
rufus173 <derrickotron5000@gmail.com>
2024-06-25 19:22:28 +0100

    created program to count the number of letters passed to it

--- source-code/count.c
+++ source-code/count.c
@@ -1,16 +1,34 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-int letter_count(int argc,char *argv[]);
+#include "args.h" //compile with args.c
+int letter_count(struct args arguments);
 int main(int argc,char *argv[]){
 	if (argc==1){
+		printf("0\n");//0 letters / words
 		return 1; //not enough arguments provided
 	}
-	printf("%d\n",letter_count(argc,argv));
+	struct args arguments; //holds all the args info
+	parse_args(argc,argv,&arguments);
+	int i = 0;
+	for (i=0;i<arguments.number_single;i++){
+		if (arguments.single[i] == 'w'){
+			;
+		}else{
+			;
+		}
+	}
+	if (i==0){
+		printf("%d\n",letter_count(arguments));
+	}
+
+	free(arguments.other); //freeing from the arg parser
 }
-int letter_count(int argc, char *argv[]){
-	int total=argc-2;
-	for (int i=1;i<argc;i++){
-		total += (int)strlen(argv[i]);
+
+int letter_count(struct args arguments){
+	int total = arguments.number_other-1;
+	for (int i=0;i<arguments.number_other;i++){
+		total += (int)strlen(arguments.other[i]);
 	}
 	return total;
 }
