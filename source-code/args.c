3b5d72db4c8172e18a43b25f53fedabb4d417fa1
rufus173 <derrickotron5000@gmail.com>
2024-06-25 18:55:56 +0100

    fixed issues with segmentation faults relating to pointer problems.

--- source-code/args.c
+++ source-code/args.c
@@ -1,10 +1,16 @@
 #include "args.h"
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 void parse_args(int argc, char *argv[], struct args *arguments){
 	int multi_count = 0; //how many single character argumens there are
 	int single_count = 0; //how many multicharacter arguments
 	int other_count = 0; // for non hyphen arguments
+	printf("creating other...\n");
+	char **other;
+	printf("alocating memory to other...\n");
+	other = (char**)malloc(sizeof(char*)*10); // pointer to array of pointers to strings
+	//printf("starting size of other %lu\n",sizeof(other));
 	for (int i=1; i<argc;i++){
 		if (argv[i][0]=='-'){//check for args starting with -
 			if (argv[i][1]=='-'){//check for args that start with --
@@ -17,10 +23,16 @@ void parse_args(int argc, char *argv[], struct args *arguments){
 				}
 			}
 		}else{//non dashed args
-			strcpy(arguments->other[other_count],argv[i]);
 			other_count++;
+			other = (char**)realloc(other,other_count*sizeof(char*));
+			printf("realocated other to %d\n",(int)(other_count*sizeof(char*)));
+			other[other_count-1] = (char*)malloc((strlen(argv[i])+1)*sizeof(char));
+			strcpy(other[other_count-1],argv[i]);
+			printf("size of new string:%ld\n",((strlen(other[other_count-1])+1))*sizeof(char));
+			printf("string stored:%s\n",other[other_count-1]);
 		}
 	}
+	arguments->other = other;
 	arguments->number_single = single_count;
 	arguments->number_multi = multi_count;
 	arguments->number_other = other_count;
--- source-code/args.h
+++ source-code/args.h
@@ -6,7 +6,7 @@ struct args{
 	int number_other; //for non - arguments
 	char single[62]; //single letter arguments (array of chars, no \0)
 	char multi[10][20]; //longer then one letter arguments
-	char other[][100];
+	char **other;
 };
 void parse_args(int argc, char *argv[],struct args *arguments);
 #endif
