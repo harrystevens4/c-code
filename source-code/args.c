da5648867f0fe9f40d1fc1b5032a55a8eadc7625
rufus173 <derrickotron5000@gmail.com>
2024-06-25 19:23:21 +0100

    removed debugging output

--- source-code/args.c
+++ source-code/args.c
@@ -6,9 +6,9 @@ void parse_args(int argc, char *argv[], struct args *arguments){
 	int multi_count = 0; //how many single character argumens there are
 	int single_count = 0; //how many multicharacter arguments
 	int other_count = 0; // for non hyphen arguments
-	printf("creating other...\n");
+	//printf("creating other...\n");
 	char **other;
-	printf("alocating memory to other...\n");
+	//printf("alocating memory to other...\n");
 	other = (char**)malloc(sizeof(char*)*10); // pointer to array of pointers to strings
 	//printf("starting size of other %lu\n",sizeof(other));
 	for (int i=1; i<argc;i++){
@@ -25,11 +25,11 @@ void parse_args(int argc, char *argv[], struct args *arguments){
 		}else{//non dashed args
 			other_count++;
 			other = (char**)realloc(other,other_count*sizeof(char*));
-			printf("realocated other to %d\n",(int)(other_count*sizeof(char*)));
+			//printf("realocated other to %d\n",(int)(other_count*sizeof(char*)));
 			other[other_count-1] = (char*)malloc((strlen(argv[i])+1)*sizeof(char));
 			strcpy(other[other_count-1],argv[i]);
-			printf("size of new string:%ld\n",((strlen(other[other_count-1])+1))*sizeof(char));
-			printf("string stored:%s\n",other[other_count-1]);
+			//printf("size of new string:%ld\n",((strlen(other[other_count-1])+1))*sizeof(char));
+			//printf("string stored:%s\n",other[other_count-1]);
 		}
 	}
 	arguments->other = other;
