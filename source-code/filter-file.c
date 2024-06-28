c83124c1d9255b49f2c5104114770c6640561679
rufus173 <derrickotron5000@gmail.com>
2024-06-28 21:40:53 +0100

    created program to output file but with ability to filter output. created -c for comment filtering

---
+++ source-code/filter-file.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "file-lines.h" //compile with file-lines.c
+#include "args.h" //compile with args.c
+int main(int argc, char* argv[]){
+	struct args arguments;
+	struct lines lines;
+	parse_args(argc,argv,&arguments);
+	/* checks */
+	if (arguments.number_other != 1){
+		fprintf(stderr,"incorrect number of arguments. expected 1 arg of <filename>\n");
+		return 1;
+	}
+	FILE* doc;
+	doc = fopen(arguments.other[0],"r");
+	if (doc == NULL){
+		fprintf(stderr,"could not open file\n");
+		return 2;
+	}
+	/* process file */
+	get_lines(&lines,doc);
+	//for (int i=0;i<lines.line_count;i++){
+	//	printf("line %d has a char count of %d and content of:%s",i,lines.line_length[i],lines.lines[i]);
+	//}
+	for (int i=0;i<arguments.number_single;i++){
+		switch (arguments.single[i]){
+			case 'c': //remove "c"oments from output
+				/* itterate through each line saving each char and stopping once a # is found. */
+				for (int line=0;line<lines.line_count;line++){
+					for (int letter=0;letter<lines.line_length[line];letter++){
+						if (lines.lines[line][letter]=='#'){
+							if (letter == 0){//full line comments
+								lines.lines[line][letter] = '\0'; //slap a null byte to terminate the string in place of the '#'
+							}else{//partial line comments
+								lines.lines[line][letter] = '\n';
+								lines.lines[line][letter+1] = '\0'; //replace newline we cut off
+							}
+						}
+					}
+				}
+		}
+	}
+	/* output */
+	for (int i=0;i<lines.line_count;i++){
+		printf("%s",lines.lines[i]);
+	}
+	/* cleanup */
+	free_args(&arguments);
+	free_lines(&lines);
+	fclose(doc);
+	return 0;
+}
