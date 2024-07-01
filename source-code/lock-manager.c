6d06cfbd4338bbc2718574683479d387d8bec43b
rufus173 <derrickotron5000@gmail.com>
2024-07-01 18:27:39 +0100

    fixed bug where tramsition would have previously left over characters

--- source-code/lock-manager.c
+++ source-code/lock-manager.c
@@ -15,6 +15,7 @@
 #define BUFFER_SIZE 12
 int start_daemon();
 int acquire_lock(char *lock_name);
+int query_lock(char *lock_name);
 void kill_daemon();
 int main(int argc, char *argv[]){
 	/* initialisation */
@@ -39,10 +40,28 @@ int main(int argc, char *argv[]){
 					 fprintf(stderr,"name not specified\n");
 					 return 1;
 				 }
-				 acquire_lock(arguments.other[0]);
-				 break;
+				 result = acquire_lock(arguments.other[0]);
+				 if (result){
+					 printf("lock taken\n");
+				 }else{
+					 printf("successfuly aquired lock\n");
+				 }
+				 return result;
 			case 'k': //kill daemon
 				  kill_daemon();
+				  break;
+			case 'q':
+				  if (arguments.number_other<1){
+					fprintf(stderr,"name not specified\n");
+					return 1;
+				  }
+				  result = query_lock(arguments.other[0]);
+				  if (result){//returns 1 if taken and 0 if free
+					  printf("lock is taken\n");
+				  }else{
+					  printf("lock is free\n");
+				  }
+				  return result;
 				  break;
 		}
 	}
@@ -77,6 +96,7 @@ int start_daemon(){
 	/* proccessing loop */
 	for(;;){ //indefinetly process connecitons
 		/* receive command */
+		memset(buffer,0,sizeof(buffer));
 		data_socket = accept(server,NULL,NULL);
 		if (data_socket < 0){
 			perror("accept");
@@ -91,18 +111,22 @@ int start_daemon(){
 		printf("received command of %s\n",command);
 		write(data_socket,confirm,7);
 		int i = 0;
+		memset(name,0,sizeof(name));
+		printf("current value for name %s\n",name);
 		for (;;){
 			i++;
 			read(data_socket,buffer,BUFFER_SIZE);
 			if (!strncmp(buffer,"END",3)){
+				memset(buffer,0,sizeof(buffer));
 				break;
 			}
 			name = realloc(name,(sizeof(char)*i)+1);
 			name[i-1] = buffer[0];
+			memset(buffer,0,sizeof(buffer));
 
 			write(data_socket,confirm,7);
 		}
-		name[i] = '\0';
+		name[i-1] = '\0';
 		printf("received name of %s\n",name);
 		//break;
 		/* command processing */
@@ -130,6 +154,16 @@ int start_daemon(){
 				lock_status = 1;
 			}
 		}
+		if (!strncmp(command,"query",5)){
+			printf("checking lock status of lock named %s\n",name);
+			for (int i=0;i<locks.number_of_locks;i++){
+				if(!strncmp(locks.lock_name[i],name,strlen(name))){
+					lock_status = 1;
+				}else{
+					lock_status = 0;
+				}
+			}
+		}
 		sprintf(buffer,"%d",lock_status);
 		printf("sending return status of %s\n",buffer);
 		result = write(data_socket,buffer,BUFFER_SIZE);
@@ -146,6 +180,60 @@ int start_daemon(){
 	free(name);
 	//exit(EXIT_SUCCESS);
 	return 0;
+}
+int query_lock(char *lock_name){
+	char buffer[BUFFER_SIZE];
+	int return_status = 2;
+	int result;
+	int data_socket;
+	printf("attempting to check status of lock of name %s\n",lock_name);
+	/* connect to server */
+	data_socket = connect_named_socket(SERVER_FD);
+	/* let the daemon know we want to aquire a lock */
+	sprintf(buffer,"query");
+	result = write(data_socket,buffer,BUFFER_SIZE);
+	if (result<0){
+		perror("write");
+		exit(EXIT_FAILURE);
+	}
+	result = read(data_socket,buffer,BUFFER_SIZE);//confirmation of receive
+	/* transmit the name of the lock */
+	for (int i=0;i<strlen(lock_name);i++){
+		printf("sending %c\n",lock_name[i]);
+		buffer[0] = lock_name[i];
+		result = write(data_socket,buffer,BUFFER_SIZE);
+		if (result<0){
+			perror("write");
+			exit(EXIT_FAILURE);
+		}
+		result = read(data_socket,buffer,BUFFER_SIZE);//confirmation of receive
+		if (result<0){
+			perror("write");
+			exit(EXIT_FAILURE);
+		}
+	}
+	printf("transmiting end of transmition\n");
+	sprintf(buffer,"END");//end of transmition signal
+	result = write(data_socket,buffer,BUFFER_SIZE);
+
+	if (result<0){
+		perror("write");
+		exit(EXIT_FAILURE);
+	}
+	for (;;){ //loop untill we receive an int and not "END"
+		read(data_socket,buffer,BUFFER_SIZE);
+		printf("got buffer for status %s\n",buffer);
+		if (strncmp(buffer,"END",3)){
+			break;
+		}
+	}
+	return_status = atoi(buffer);
+	printf("returned with status %d\n",return_status);
+
+	/* clean up */
+	printf("cleaning up\n");
+	close(data_socket);
+	return return_status;
 }
 int acquire_lock(char *lock_name){
 	char buffer[BUFFER_SIZE];
@@ -199,7 +287,7 @@ int acquire_lock(char *lock_name){
 	/* clean up */
 	printf("cleaning up\n");
 	close(data_socket);
-	return 0;
+	return return_status;
 }
 void kill_daemon(){
 	char buffer[BUFFER_SIZE];
