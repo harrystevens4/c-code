76b227853e824ac540ad47de24eb08144ccebd0e
rufus09173 <rufus173>
2024-07-29 23:20:25 +0100

    started on features to analyse the script it is piped from to automaticaly calculate the max line output

--- source-code/progress.c
+++ source-code/progress.c
@@ -16,18 +16,51 @@ int main(int argc, char **argv){
 	int buffer_size = BUFFER_SIZE;
 	char *data;
 	char buffer[buffer_size];
+	char command_buffer[1024];
+	int pid = 660;
+	char executable_location[1024];
+	char *result;
+	FILE *output_file;
 	ioctl(STDOUT_FILENO,TIOCGWINSZ, &window_size);
 	width = window_size.ws_col;
 	total_lines = 0;
 	struct args args;
 	parse_args(argc,argv,&args);
+	if ((args.number_single > 0) && (args.number_other > 0)){
+		if (args.single[0] == 'c'){
+			total_lines = (int)strtol(args.other[0],(char **)NULL, 10);
+		}
+
+	}else{
+		//analyse script on otherside of pipe to attempt to decipher total_lines
+		printf("getppid: %d\n",getppid());
+		pid = getpgrp();
+		printf("attempting to analyse executable...\n");
+		sprintf(command_buffer,"readlink -f /proc/%d/exe",pid);//get executable location
+		printf("attempting to run %s\n",command_buffer);
+		output_file = popen(command_buffer,"r");
+		result = fgets(executable_location,1024,output_file);
+		if (result != NULL){
+			if (output_file == NULL){
+				printf("something went wrong whilst executing %s\n",command_buffer);
+			}
+			if (feof(output_file)){
+				printf("could not deduce executable location\n");
+			}
+		}else{
+			printf("no executable location found\n");
+		}
+		printf("%s",executable_location);
+		
+		pclose(output_file);
+	}
 	for (;;){
 		if ((args.number_other == 0) && (args.number_single == 0)){
 			total_lines++;
 		}
 		data = fgets(buffer, sizeof(buffer), stdin);
 		if (data == NULL){// if the pipe is closed, stop
-			return 0;
+			break;
 		}
 		if (buffer[strlen(buffer)-1] == '\n'){
 			buffer[strlen(buffer)-1] = '\0';
