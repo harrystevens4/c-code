e7d8e040307543935fc099dad28400a1c6594146
rufus09173 <rufus173>
2024-08-17 17:54:17 +0100

    finnished connect_server_socket and make_server_socket

--- source-code/tcp-toolkit.c
+++ source-code/tcp-toolkit.c
@@ -9,7 +9,7 @@
 
 int verbose_tcp_toolkit;
 
-int make_socket(const char * port, struct addrinfo **res){
+int make_socket(const char * host ,const char * port, struct addrinfo **res){
 	int socketfd;
 
 	//set up info for socket
@@ -24,7 +24,7 @@ int make_socket(const char * port, struct addrinfo **res){
 	hints.ai_family = AF_INET;
 	hints.ai_socktype = SOCK_STREAM; //byte order matters
 
-	int result = getaddrinfo(NULL, port, &hints, res); //fill in addrinfo struct 'res'
+	int result = getaddrinfo(host, port, &hints, res); //fill in addrinfo struct 'res'
 	if (result != 0){
 		fprintf(stderr,"ERROR [tcp-toolkit/make_socket]: getaddrinfo: %s",gai_strerror(result));
 		return -1;
@@ -53,8 +53,9 @@ int make_socket(const char * port, struct addrinfo **res){
 }
 int make_server_socket(const char * port, int backlog){
 	struct addrinfo *res;
-	int socket = make_socket(port, &res);
+	int socket = make_socket(NULL, port, &res);
 	if (socket < 0){
+		fprintf(stderr, "ERROR: could not make the socket\n");
 		return -1;
 	}
 
@@ -73,8 +74,37 @@ int make_server_socket(const char * port, int backlog){
 	if (verbose_tcp_toolkit){
 		printf("[tcp-toolkit/make_server_socket]: Starting listener.\n");
 	}
-	if (listen(socket,backlog))
+	if (listen(socket,backlog) < 0){
+		fprintf(stderr,"ERROR [tcp-toolkit/make_server_socket]: Could not listen.\n");
+		perror("listen");
+		return -1;
+	}
+	if (verbose_tcp_toolkit){
+		printf("[tcp-toolkit/make_server_socket]: Listening...\n");
+	}
 
 	//cheeky bit of cleanup
 	freeaddrinfo(res);
+	return socket;
+}
+int connect_server_socket(char * host, char * port){
+	struct addrinfo *res;
+	int socket = make_socket(NULL, port, &res);
+	if (socket < 0){
+		fprintf(stderr, "ERROR [tcp-toolkit/connect_server_socket]: could not make the socket\n");
+		return -1;
+	}
+	if (verbose_tcp_toolkit){
+		printf("[tcp-toolkit/connect_server_socket]: Attempting to connect...\n");
+	}
+	if (connect(socket,res->ai_addr, res->ai_addrlen) < 0){
+		fprintf(stderr,"ERROR [tcp-toolkit/connect_server_socket]: Could not connect.\n");
+		perror("connect");
+		return -1;
+	}
+	if (verbose_tcp_toolkit){
+		printf("[tcp-toolkit/connect_server_socket]: Connection successful\n");
+	}
+
+	return socket;
 }
--- source-code/tcp-toolkit.h
+++ source-code/tcp-toolkit.h
@@ -2,6 +2,7 @@
 #define TCP_TOOLKIT_H
 
 int make_server_socket(const char * port, int backlog);//sets up a listening server socket
+int connect_server_socket(char * host, char * port);
 
 extern int verbose_tcp_toolkit; //debugging info from tcp-toolkit functions
 
