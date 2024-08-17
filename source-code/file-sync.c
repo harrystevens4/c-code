8cd3cbbddf5e64c97ce4f0351324751402f82835
rufus09173 <rufus173>
2024-08-17 17:55:16 +0100

    added client and server connection functions.

--- source-code/file-sync.c
+++ source-code/file-sync.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <sys/socket.h>
 #include "tcp-toolkit.h"
 #include "args.h"
@@ -6,6 +7,7 @@
 #define PORT "1173"
 
 int server(); //server holds and sends files to clients
+int client(); //receives files from the server
 
 int main(int argc, char **argv){
 	verbose_tcp_toolkit = 1;//output from tcp toolkit funcitons
@@ -13,8 +15,10 @@ int main(int argc, char **argv){
 	parse_args(argc,argv,&args);
 	printf("starting...\n");
 	switch (args.single[0]){
-		case ('s'):
+		case 's':
 			return server();
+		case 'c':
+			return client();
 	}
 	free_args(&args);
 	return 0;
@@ -24,7 +28,33 @@ int server(){
 	int client;
 	int server;
 
+	struct sockaddr *client_addr;
+	socklen_t *client_addrlen;
+
 	printf("server mode selected\n");
 	server = make_server_socket(PORT, 10);//create socket, bind and start listening
-	//client = accept();
+	if (server < 0){
+		fprintf(stderr,"ERROR: Could not start server socket.\n");
+		return -1;
+	}
+	client = accept(server,client_addr,client_addrlen);
+	printf("connection accepted.\n");
+
+	//cleanup
+	close(client);
+}
+
+int client(){
+	int server;
+
+	struct sockaddr *client_addr;
+	socklen_t *client_addrlen;
+
+	printf("client mode selected\n");
+	server = connect_server_socket(NULL,PORT);
+	if (server < 0){
+		fprintf(stderr,"ERROR: Could not connect.\n");
+		return -1;
+	}
+	printf("Connection accepted.\n");
 }
