5f5c3986336027291ca33eda70faf4023a91ee13
rufus09173 <rufus173>
2024-08-24 23:46:04 +0100

    added help text

--- source-code/network-search.c
+++ source-code/network-search.c
@@ -1,11 +1,36 @@
 #include <stdio.h>
 #include "tcp-toolkit.h"
+#include "args.h"
+void print_help(){
+	printf("Usage:\n");
+	printf("	network-search [options] [port]\n");
+	printf("	-b : broadcast\n");
+	printf("	-f : find active broadcasts\n");
+	printf("Return values:\n");
+	printf("	error : exit status 1\n");
+	printf("	success : ip address of found broadcast\n");
+}
 int main(int argc, char **argv){
 	verbose_tcp_toolkit = 1;
 	silent_errors = 0;
-	if (argc > 1){
-		printf("returned %d\n",broadcast_existence("8500"));
-	}else{
-		printf("got broadcaster at %s\n",find_broadcasters("8500"));
+	if (argc < 2){
+		print_help();
+		return 1;
 	}
+	struct args args;
+	parse_args(argc,argv,&args);
+	if (args.number_other != 1 || args.number_single != 1){
+		print_help();
+		return 1;
+	}
+	if (args.single[0] == 'b'){
+		broadcast_existence(args.other[0]);
+	}else if (args.single[0] == 'f'){
+		printf("%s\n",find_broadcasters(args.other[0]));
+	}else{
+		print_help();
+		return 1;
+	}
+	free_args(&args);
+	
 }
