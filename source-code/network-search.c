1c78c3c913aef9f1bd183e1935cfb0ff574c18bc
rufus09173 <rufus173>
2024-08-24 23:23:07 +0100

    renamed in order to repurpose to new tool

---
+++ source-code/network-search.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "tcp-toolkit.h"
+int main(int argc, char **argv){
+	verbose_tcp_toolkit = 1;
+	silent_errors = 0;
+	if (argc > 1){
+		printf("returned %d\n",broadcast_existence("8500"));
+	}else{
+		printf("got broadcaster at %s\n",find_broadcasters("8500"));
+	}
+}
