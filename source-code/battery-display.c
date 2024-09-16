9506d195dab8650ebf5f9effc221cbb5433601fc
rufus173 <rufus09173@gmail.com>
2024-09-16 13:57:01 +0100

    added funciton to get battery percent

--- source-code/battery-display.c
+++ source-code/battery-display.c
@@ -1,6 +1,38 @@
 #include <stdio.h>
 #include <gtk/gtk.h>
-int get_batter_level(){
+#include <unistd.h>
+int get_battery_percent(){
+	if (access("/sys/class/power_supply/BAT0/charge_now",F_OK) != 0){
+		fprintf(stderr,"ERROR: could not open the battery level file.\n");
+		return -1;
+	}
+	
+	FILE *charge_now_fd = fopen("/sys/class/power_supply/BAT0/charge_now","r");
+	FILE *charge_full_fd = fopen("/sys/class/power_supply/BAT0/charge_full","r");
+	if (charge_now_fd == NULL || charge_full_fd == NULL){
+		fprintf(stderr,"ERROR: Could not open battery charge file descriptors\n");
+		perror("fopen");
+		return -1;
+	}
+	
+	char buffer[1024];
+	int buffer_size = fread(buffer,1,1024,charge_now_fd);
+	if (buffer_size == 0){
+		fprintf(stderr,"ERROR: Could not read file.\n");
+	}
+	int charge_now = strtol(buffer,NULL,10);
+	
+	buffer_size = fread(buffer,1,1024,charge_full_fd);
+	if (buffer_size == 0){
+		fprintf(stderr,"ERROR: Could not read file.\n");
+	}
+	int charge_full = strtol(buffer,NULL,10);
+
+	double percentage = ((double)charge_now/(double)charge_full);
+	percentage = percentage*100;
+	fclose(charge_now_fd);
+	fclose(charge_full_fd);
+	return percentage;
 }
 int activate(GtkApplication *app,gpointer user_data){
 	
@@ -14,7 +46,9 @@ int activate(GtkApplication *app,gpointer user_data){
 	gtk_widget_set_valign(box, GTK_ALIGN_CENTER);
 
 	//make widgets
-	GtkWidget *label = gtk_label_new("Battery level: ");
+	char label_string[1024];
+	snprintf(label_string,1024,"Battery level: %d%%",get_battery_percent());
+	GtkWidget *label = gtk_label_new(label_string);
 	GtkWidget *battery_level_bar_widget = gtk_level_bar_new();
 	GtkLevelBar *batter_level_bar = GTK_LEVEL_BAR(battery_level_bar_widget);
 
