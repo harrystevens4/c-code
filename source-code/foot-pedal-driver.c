f35fc2bccaee0e55a0e6b564e0a8720659fe10ba
rufus173 <derrickotron5000@gmail.com>
2024-12-28 20:53:00 +0000

    added keypress simulation via Xlib

--- source-code/foot-pedal-driver.c
+++ source-code/foot-pedal-driver.c
@@ -3,6 +3,9 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/stat.h>
+#include <X11/Xlib.h>
+#include <X11/keysym.h>
+int simulate_keypress(char *key, int pressed);
 int get_pedal_state(int pedal_fd);
 int main(int argc, char **argv){
 	char *device = "/dev/ttyACM0";
@@ -12,15 +15,21 @@ int main(int argc, char **argv){
 		return 1;
 	}
 	for (;;){
-		int pedal_state = get_pedal_state(pedal_fd);
-		if (pedal_state < 0){
+		static int previous_pedal_state = 0;
+		static int new_pedal_state = 0;
+		new_pedal_state = get_pedal_state(pedal_fd);
+		if (new_pedal_state < 0){
 			fprintf(stderr,"main(): get_pedal_state error\n");
 			close(pedal_fd);
 			return 1;	
 		}
 		//lets not waste system resources
 		//usleep(100000); //100,000us = 0.1s
-		printf("%d\n",pedal_state);
+		if (previous_pedal_state != new_pedal_state){
+			printf("%d\n",new_pedal_state);
+			simulate_keypress("Control_L",new_pedal_state);
+		}
+		previous_pedal_state = new_pedal_state;
 	}
 	close(pedal_fd);
 	return 0;
@@ -34,4 +43,34 @@ int get_pedal_state(int pedal_fd){
 		return -1;
 	}
 	return strtol(state,NULL,10);
+}
+int simulate_keypress(char *key, int pressed){
+	Display *display = XOpenDisplay(NULL);
+	if (display == NULL){
+		fprintf(stderr,"Could not connect to X display.\n");
+		return -1;
+	}
+	Window root_window = XDefaultRootWindow(display);
+	int revert; //we dont use this
+	Window focused_window;
+	int result = XGetInputFocus(display,&focused_window,&revert);
+	
+	//im manualy packing the struct
+	XKeyEvent key_event = {
+		.display = display,
+		.window = focused_window,
+		.root = root_window,
+		.subwindow = None,
+		.time = CurrentTime,
+		.x = 1,
+		.y = 1,
+		.x_root = 1,
+		.y_root = 1,
+		.same_screen = True,
+		.keycode = XKeysymToKeycode(display,XStringToKeysym(key)),
+		.state = 0,
+		.type = (pressed == 1) ? KeyPress : KeyRelease,
+	};
+	XSendEvent(display,focused_window,True,KeyPressMask,(XEvent *)&key_event);
+	XCloseDisplay(display);
 }
