17bfc9a229c59e2f9e056207ba0d2482cbf03d5e
rufus173 <derrickotron5000@gmail.com>
2024-12-29 17:56:27 +0000

    fixed initialisation issues

--- source-code/foot-pedal-driver.c
+++ source-code/foot-pedal-driver.c
@@ -2,9 +2,11 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <termios.h>
 #include <sys/stat.h>
 #include <X11/Xlib.h>
 #include <X11/keysym.h>
+int connect_pedal(char *device);
 int simulate_keypress(char *key, int pressed);
 int get_pedal_state(int pedal_fd);
 int main(int argc, char **argv){
@@ -13,9 +15,9 @@ int main(int argc, char **argv){
 		return 1;
 	}
 	char *device = argv[1];
-	int pedal_fd = open(device,O_RDONLY);
+	int pedal_fd = connect_pedal(device);//open(device,O_RDONLY);
 	if (pedal_fd < 0){
-		perror("open");
+		fprintf(stderr,"could not open the serial line.\n");
 		return 1;
 	}
 	for (;;){
@@ -38,6 +40,52 @@ int main(int argc, char **argv){
 	}
 	close(pedal_fd);
 	return 0;
+}
+int connect_pedal(char *device){
+	//open device
+	int pedal_fd = open(device,O_RDONLY);
+	if (pedal_fd < 0){
+		perror("open");
+		return -1;
+	}
+
+	//set baud rate
+	struct termios options;
+	int result = tcgetattr(pedal_fd, &options); //fill in the current options
+	if (result < 0){perror("tcgetattr");return -1;}
+	
+	result = cfsetispeed(&options,B9600);
+	if (result < 0){perror("cfsetispeed");return -1;}
+
+	result = cfsetospeed(&options,B9600);
+	if (result < 0){perror("cfsetospeed");return -1;}
+	
+	//set other important attributes
+	options.c_cflag &= ~PARENB;
+	options.c_cflag &= ~CSTOPB;
+	options.c_cflag &= ~CSIZE;
+	options.c_cflag |= CS8;
+	// no flow control
+	options.c_cflag &= ~CRTSCTS;
+	options.c_cflag |= CREAD | CLOCAL;  // turn on READ & ignore ctrl lines
+	options.c_iflag &= ~(IXON | IXOFF | IXANY); // turn off s/w flow ctrl
+	options.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG); // make raw
+	options.c_oflag &= ~OPOST; // make raw
+
+	//timings for bytes in control sequences
+	options.c_cc[VMIN]  = 0;
+	options.c_cc[VTIME] = 20;
+
+	result = tcsetattr(pedal_fd,TCSANOW,&options); //make changes immediately
+	if (result < 0){perror("tcsetattr");return -1;}
+
+	tcgetattr(pedal_fd, &options);
+	printf("baud: %d in, %d out, should be %d\n",cfgetispeed(&options),cfgetospeed(&options),B9600);
+
+	result = tcflush(pedal_fd,TCIOFLUSH); //flush any data
+	if (result < 0){perror("tcflush");return -1;}
+
+	return pedal_fd;
 }
 int get_pedal_state(int pedal_fd){
 	char state[2] = "0";
