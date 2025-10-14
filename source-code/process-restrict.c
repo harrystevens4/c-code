10a789583feeeb509af43e36ec716dfc3cb13c8c
rufus173 <rufus09173@gmail.com>
2025-10-14 14:47:50 +0100

    added LD_PRELOAD environ removal protection

--- source-code/process-restrict.c
+++ source-code/process-restrict.c
@@ -1,4 +1,5 @@
 #include <dlfcn.h>
+#include <stdlib.h>
 #include <libgen.h>
 #include <sys/stat.h>
 #include <string.h>
@@ -7,15 +8,17 @@
 #include <errno.h>
 
 int (*old_execve)(const char *,char *const *argv,char *const *envp);
-char banned_commands[][256] = {"java","node"};
+char banned_commands[][256] = {"java","node","ssh"};
+char *ld_preload_value;
 
 void __attribute__((__constructor__)) init(){
 	//store the old execve
 	old_execve = dlsym(RTLD_NEXT,"execve");
+	ld_preload_value = strdup(getenv("LD_PRELOAD"));
 }
 
 static int check_if_allowed(const char *path, char * const* argv, char * const* envp){
-	//====== resolve symboliclinks ======
+	//====== resolve symbolic links ======
 	char resolved_link[4097] = {0};
 	memset(resolved_link,0,sizeof(resolved_link));
 	if (readlink(path,resolved_link,sizeof(resolved_link)) < 0){
@@ -25,6 +28,25 @@ static int check_if_allowed(const char *path, char * const* argv, char * const* 
 	struct stat statbuf = {0};
 	if (stat(resolved_link,&statbuf) < 0) return -1;
 	if ((statbuf.st_mode & (S_ISGID | S_ISUID)) != 0){
+		errno = EPERM;
+		return -1;
+	}
+	//====== check LD_PRELOAD is untampered with ======
+	int ld_preload_found = 0;
+	for (int i = 0; envp[i] != NULL; i++){
+		char *env_var = envp[i];
+		//compare everything before the '=' to LD_PRELOAD
+		if (strncmp("LD_PRELOAD",env_var,strchr(env_var,'=')-env_var) == 0){
+			ld_preload_found = 1;
+			if (strcmp(strchr(env_var,'=')+1,ld_preload_value) != 0){
+				//LD_PRELOAD has been tampered with
+				errno = EPERM;
+				return -1;
+			}
+			printf("%s == %s\n",strchr(env_var,'=')+1,ld_preload_value);
+		}
+	}
+	if (!ld_preload_found){
 		errno = EPERM;
 		return -1;
 	}
