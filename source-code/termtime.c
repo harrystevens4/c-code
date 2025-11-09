ee98335f2d7b71b394c8e8919abe5b06ccae6056
rufus173 <derrickotron5000@gmail.com>
2025-11-09 22:14:40 +0000

    implemented ascii font rendering

--- source-code/termtime.c
+++ source-code/termtime.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <string.h>
+#include <time.h>
 #include <unistd.h>
 #include <sys/ioctl.h>
 #include <termios.h>
@@ -8,7 +10,8 @@
 #include <locale.h>
 #include <fcntl.h>
 
-char *get_character(char c);
+char *get_character_glyph(char c);
+int draw_text(int y, int x, char *text);
 
 int main(int argc, char **argv){
 	//====== get ncurses set up ======
@@ -56,8 +59,8 @@ int main(int argc, char **argv){
 		perror("tcsetattr");
 		return 1;
 	}
-	//set the first alarm
-	alarm(1);
+	//start the alarm loop (alarm() cannot create a 0 second alarm)
+	kill(getpid(),SIGALRM);
 	//====== update loop ======
 	for (;;){
 		//====== wait for signal ======
@@ -80,6 +83,8 @@ int main(int argc, char **argv){
 				break;
 			}
 			if (input == 'q') break;
+			//no need to re render the clock
+			continue;
 		}else if (signal == SIGALRM){
 			//update the time
 			//schedule update for 1 second later
@@ -88,18 +93,59 @@ int main(int argc, char **argv){
 			//exit
 			break;
 		}
+		//====== get the time ======
+		time_t timestamp = time(NULL);
+		struct tm *time_info = localtime(&timestamp);
 		//====== redraw the clock ======
-		//erase();
+		erase();
+		//terminal too small?
+		if (LINES < 9){
+			printw("Please increase the size of the window");
+			refresh();
+			continue;
+		}
+		//render the text
+		char timestring_buffer[1024] = {0};
+		size_t timestring_bufferlen = strftime(
+			timestring_buffer,
+			sizeof(timestring_buffer),
+			"%H %M %S",
+			time_info
+		);
+		//                   each letter is 10 chars wide (with one space at the end)
+		int text_x = (COLS/2)-((timestring_bufferlen*(10+1))/2);
+		//        each letter is 9 chars tall
+		int text_y = (LINES/2) - 9/2;
+		if (text_x < 0) text_x = 0;
+		draw_text(text_y,text_x,timestring_buffer);
 		refresh();
 	}
 	//====== shut everything down ======
 	endwin();
-	for (int i = 0; i < 10; i++) printf("%s\n",get_character('0'+i));
+	//for (int i = 0; i < 10; i++) printf("%s\n",get_character('0'+i));
 	return 0;
 }
 
+int draw_text(int y, int x, char *text){
+	mvprintw(y,x,"%s",text);
+	//for each character in the string
+	for (char *character = text; *character != '\0'; character++){
+		//convert to glyph
+		char *glyph = get_character_glyph(*character);
+		//for each line in the glyph
+		int current_y = 0;
+		for (char *line = glyph; line-1 != NULL; line = strchr(line,'\n')+1){
+			long line_length = (strchr(line,'\n') != NULL) ? (strchr(line,'\n')-line) : strlen(line);
+			mvaddnstr(y+current_y,x,line,line_length);
+			current_y++;
+		}
+		//move cursor across
+		x += 11;
+	}
+}
+
 //get the ascii font for that specific character
-char *get_character(char c){
+char *get_character_glyph(char c){
 	if (c >= '0' && c <= '9'){
 		return (char *[]){
 			"\
