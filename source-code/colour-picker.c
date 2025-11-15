dcc1ba4f5c6f976941cfed90ab1ac630eaa1c420
rufus173 <derrickotron5000@gmail.com>
2025-11-15 22:57:52 +0000

    smooth colour blending acheived!!!!!!

--- source-code/colour-picker.c
+++ source-code/colour-picker.c
@@ -31,10 +31,10 @@ int main(int argc, char **argv){
 	//        squish y due to terminal characters being tall
 	for (int y = 0; y < term_height*2; y+=2){
 		for (int x = 0; x < term_width; x++){
-			//calculate colour of coordinate
-			uint8_t red = 0;
-			uint8_t green = 0;
-			uint8_t blue = 0;
+			//calculate "magic value" of each colour
+			double red = 0;
+			double green = 0;
+			double blue = 0;
 			for (int i = 0; i < point_count; i++){
 				//perpendicular distance to the tangent at point points[i] of circle with radius "radius"
  				//pythagoras
@@ -50,31 +50,32 @@ int main(int argc, char **argv){
 					break;
 				}
 				double centre_distance = sqrt(
-					pow((double)((term_width/2)-points[i][0]),2) + 
-					pow((double)((term_height)-points[i][1]),2)
+					pow((double)((term_width/2)-x),2) + 
+					pow((double)((term_height)-y),2)
 				);
 				//cosine rule
 				double cos_angle = (
 					(pow(centre_distance,2)-pow(point_distance,2)-pow(radius,2))
 					                /
-					((-2)*radius*point_distance)
+					(-2*radius*point_distance)
 				);
-				double max_perpendicular_distance = cos((M_PI)/point_count)*radius*2;
+				double max_perpendicular_distance = cos((M_PI)/point_count)*radius+radius*2;
 				//what proportion of colour?
 				double perpendicular_distance = cos_angle*point_distance;
-				double proportion = (perpendicular_distance/max_perpendicular_distance);
-				if (proportion > 1) proportion = 1;
+				double magic_distance = max_perpendicular_distance-perpendicular_distance;
+				if (magic_distance < 0) magic_distance = 0;
 				//set the colour
-				if (i == 0) red   = UINT8_MAX-(UINT8_MAX*proportion);
-				if (i == 1) green = UINT8_MAX-(UINT8_MAX*proportion);
-				if (i == 2) blue  = UINT8_MAX-(UINT8_MAX*proportion);
+				if (i == 0) red   = magic_distance;//UINT8_MAX-(UINT8_MAX*proportion);
+				if (i == 1) green = magic_distance;//UINT8_MAX-(UINT8_MAX*proportion);
+				if (i == 2) blue  = magic_distance;//UINT8_MAX-(UINT8_MAX*proportion);
 			}
-			colour_grid[(y/2)*term_width*3 + x*3 + 0] = red;
-			colour_grid[(y/2)*term_width*3 + x*3 + 1] = green;
-			colour_grid[(y/2)*term_width*3 + x*3 + 2] = blue;
+			double rgb_total = red + green + blue;
+			colour_grid[(y/2)*term_width*3 + x*3 + 0] = (red   /rgb_total)*UINT8_MAX;
+			colour_grid[(y/2)*term_width*3 + x*3 + 1] = (green /rgb_total)*UINT8_MAX;
+			colour_grid[(y/2)*term_width*3 + x*3 + 2] = (blue  /rgb_total)*UINT8_MAX;
 		}
 	}
-	for (int y = 0; y < term_height; y++){
+	for (int y = 0; y < term_height-1; y++){
 		for (int x = 0; x < term_width; x++){
 			uint8_t red   = colour_grid[y*term_width*3 + x*3 + 0];
 			uint8_t green = colour_grid[y*term_width*3 + x*3 + 1];
@@ -84,7 +85,6 @@ int main(int argc, char **argv){
 		}
 		if (y < term_height-1) printf("\n");
 	}
-	sleep(5);
 	free(colour_grid);
 }
 
