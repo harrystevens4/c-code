6d89f1d3631271954f9a2fb122c39c1042b5a895
harry <harrywstevens4@gmail.com>
2026-03-27 23:20:15 +0000

    fixed segmentation fault due to % instead of %=

--- install/dnswall/src/main.c
+++ install/dnswall/src/main.c
@@ -20,8 +20,22 @@ struct __attribute__((packed)) dns_header {
 	uint16_t additional_rr_count;
 };
 
+struct dns_response {
+	uint16_t transaction_id;
+	struct sockaddr *dest_addr;
+	socklen_t dest_addrlen;
+};
+
+#define MAX_TRANSACTION_IDS 50
+//ring buffer
+uint16_t recent_transaction_ids[MAX_TRANSACTION_IDS] = {0};
+int recent_transaction_ids_head = 0;
+
 int set_promisc_mode(int socket, const char *interface, int state);
 const char *protocol_name_from_number(int protocol);
+int dns_send_response(int send_fd, struct dns_response *response);
+void add_recent_transaction_id(uint16_t id);
+int is_transaction_id_recent(uint16_t id);
 
 int main(int argc, char **argv){
 	if (argc < 2){
@@ -29,15 +43,30 @@ int main(int argc, char **argv){
 		return 1;
 	}
 	const char *interface = argv[1];
-	//====== setup socket ======
+	//====== setup sockets ======
 	int monitor_fd = socket(AF_PACKET,SOCK_DGRAM,htons(ETH_P_ALL));
 	if (monitor_fd < 0){
 		perror("socket");
 		return 1;
 	}
+	int ipv4_send_fd = socket(AF_INET,SOCK_DGRAM,0);
+	if (ipv4_send_fd < 0){
+		perror("socket");
+		close(monitor_fd);
+		return 1;
+	}
+	int ipv6_send_fd = socket(AF_INET6,SOCK_DGRAM,0);
+	if (ipv6_send_fd < 0){
+		perror("socket");
+		close(monitor_fd);
+		close(ipv4_send_fd);
+		return 1;
+	}
 	//====== enable promiscuous mode ======
 	int result = set_promisc_mode(monitor_fd,interface,1);
 	if (result != 0){
+		close(ipv4_send_fd);
+		close(ipv6_send_fd);
 		return 1;
 	}
 	//====== listen for incomming packets ======
@@ -53,6 +82,8 @@ int main(int argc, char **argv){
 		if (packet_size < 0){
 			perror("recvfrom");
 			close(monitor_fd);
+			close(ipv4_send_fd);
+			close(ipv6_send_fd);
 			return 1;
 		}
 
@@ -63,6 +94,8 @@ int main(int argc, char **argv){
 		if (result < 0){
 			perror("recvfrom");
 			close(monitor_fd);
+			close(ipv4_send_fd);
+			close(ipv6_send_fd);
 			free(packet);
 			return 1;
 		}
@@ -122,9 +155,9 @@ int main(int argc, char **argv){
 			printf("==> IPv6 carying protocol %s\n",protocol_name_from_number(transport_protocol));
 			//sender and recipient
 			memcpy(&((struct sockaddr_in6 *)&src_addr)->sin6_addr,packet+12,sizeof(struct in6_addr));
-			src_addrlen = sizeof(struct sockaddr_in);
+			src_addrlen = sizeof(struct sockaddr_in6);
 			memcpy(&((struct sockaddr_in6 *)&dest_addr)->sin6_addr,packet+16,sizeof(struct in6_addr));
-			dest_addrlen = sizeof(struct sockaddr_in);
+			dest_addrlen = sizeof(struct sockaddr_in6);
 			src_addr.ss_family = AF_INET6;
 			dest_addr.ss_family = AF_INET6;
 			printf("==> From [%s] To [%s]\n",
@@ -166,23 +199,72 @@ int main(int argc, char **argv){
 			dest_addr_in6->sin6_port = htons(dest_port);
 			break;
 		}
+		char *udp_payload = ip_payload + 8;
+		size_t udp_payload_size = ntohs(*(uint16_t *)(ip_payload+4)) - 8;
+		printf("==> Payload size [%lu]\n",udp_payload_size);
+		//====== process dns requests ======
+		//discard all non port 53 traffic
+		if (dest_port != 53){
+			free(packet);
+			continue;
+		}
+		//read dns headers
+		struct dns_header *dns_header = (struct dns_header *)udp_payload;
+		struct dns_response response = {0};
+		//ignore responses we send
+		if (is_transaction_id_recent(dns_header->transaction_id)){
+			free(packet);
+			continue;
+		}
+		//construct response
+		printf("\033[32m<====> DNS packet detected <====>\033[39m\n");
+		printf("==> Transaction id [%u]\n",ntohs(dns_header->transaction_id));
+		response.transaction_id = dns_header->transaction_id;
+		response.dest_addr = (struct sockaddr *)&src_addr;
+		response.dest_addrlen = src_addrlen;
+		int sending_socket = (dest_addr.ss_family == AF_INET) ? ipv4_send_fd : ipv6_send_fd;
+		result = dns_send_response(sending_socket,&response);
+		if (result < 0){
+			fprintf(stderr,"Failed to send dns response\n");
+		}
+		//record that the request has been responded to
+		add_recent_transaction_id(dns_header->transaction_id);
+		char addr_string_buffer[64] = {0};
+		switch (response.dest_addr->sa_family){
+		case AF_INET:
+			inet_ntop(AF_INET,&((struct sockaddr_in *)(response.dest_addr))->sin_addr,addr_string_buffer,sizeof(addr_string_buffer));
+			break;
+		case AF_INET6:
+			inet_ntop(AF_INET6,&((struct sockaddr_in6 *)(response.dest_addr))->sin6_addr,addr_string_buffer,sizeof(addr_string_buffer));
+			break;
+		}
+		printf("<== Sent response to [%s:%u], [%ld] bytes\n",addr_string_buffer,src_port,result);
 		//====== cleanup ======
 		free(packet);
 	}
 	return 0;
 }
 
-int dns_send_response(void){
+void add_recent_transaction_id(uint16_t id){
+	recent_transaction_ids[recent_transaction_ids_head] = id;
+	recent_transaction_ids_head++;
+	recent_transaction_ids_head %= MAX_TRANSACTION_IDS;
+}
+
+int is_transaction_id_recent(uint16_t id){
+	for (int i = 0; i < MAX_TRANSACTION_IDS; i++){
+		if (recent_transaction_ids[i] == id) return 1;
+	}
+	return 0;
+}
+
+int dns_send_response(int send_fd, struct dns_response *response_info){
 	//====== sending address ======
-	struct sockaddr_in broadcast_address = {
-		.sin_family = AF_INET,
-		.sin_port = htons(53),
-		.sin_addr = htonl(0xffffffff),
-	};
-	int send_fd = 0;
+	struct sockaddr *dest_addr = response_info->dest_addr;
+	socklen_t dest_addrlen = response_info->dest_addrlen;
 	//====== send responses back ======
 	struct dns_header response = {
-		.transaction_id = 0,
+		.transaction_id = response_info->transaction_id,
 		.QR_OPCODE_AA_TC_RD = htobe16(0b1000010),
 		.RA_Z_AD_CD_RCODE = htobe16(0b110110000),
 		.question_count = 0,
@@ -190,12 +272,12 @@ int dns_send_response(void){
 		.authority_rr_count = 0,
 		.additional_rr_count = 0,
 	};
-	int result = sendto(send_fd,&response,sizeof(response),0,(struct sockaddr *)&broadcast_address,sizeof(broadcast_address));
+	int result = sendto(send_fd,&response,sizeof(response),0,dest_addr,dest_addrlen);
 	if (result < 0){
-		perror("send");
-		close(send_fd);
-		return 1;
+		perror("sendto");
+		return -1;
 	}
+	return result;
 }
 
 int set_promisc_mode(int socket, const char *interface, int state){
